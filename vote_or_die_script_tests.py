4727c1a96101dcda84170a4ea82b41d9021b166f
Evgeny Golyshev <eugulixes@gmail.com>
2019-03-25 23:40:38 +0300

    Make vote_or_die_script_tests.py meet pylint requirements

--- vote_or_die_script_tests.py
+++ vote_or_die_script_tests.py
@@ -13,24 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Tests related to the hubot-vote-or-die script. """
+
 import sys
 import time
 
-from optparse import OptionParser
+from optparse import OptionParser  # pylint: disable=deprecated-module
 
 from base import RocketChatTestCase
 
 
 class VoteOrDieScriptTestCase(RocketChatTestCase):
+    """Tests for the hubot-vote-or-die script. """
+
     def __init__(self, addr, username, password, **kwargs):
         RocketChatTestCase.__init__(self, addr, username, password, **kwargs)
 
         self.schedule_pre_test_case('choose_general_channel')
 
     def _wait_value(self, css_selector, position, expected_value, retries=30):
-        for i in range(retries):
+        for _ in range(retries):
             elem_list = self.find_by_css(css_selector)
-            assert len(elem_list)
+            assert elem_list
 
             elem = elem_list[position]
             if elem.value == expected_value:
@@ -39,17 +43,27 @@ class VoteOrDieScriptTestCase(RocketChatTestCase):
         return False
 
     def test_creating_poll_with_1_option(self):
+        """Tests if it's not possible to create a poll with 1 option. The polls
+        require more than 1 options.
+        """
+
         self.send_message('!poll question?, option 1')
 
         assert self.check_latest_response_with_retries('Provide more than one option.')
 
     def test_creating_poll_with_2_options(self):
+        """Tests if it's possible to create a poll with 2 options. """
+
         self.send_message('!poll question?, option 1, option 2')
 
         assert self.check_latest_response_with_retries(
             '_Please vote using reactions_\nquestion?\n0⃣ option 1\n1⃣ option 2')
 
     def test_creating_poll_with_3_options_and_check_related_emojis(self):
+        """Tests if it's possible to create a poll with 3 options and checks if
+        there're the corresponding emojis.
+        """
+
         self.send_message('!poll question?, option 1, option 2, option 3')
 
         assert self.check_latest_response_with_retries(
@@ -58,6 +72,10 @@ class VoteOrDieScriptTestCase(RocketChatTestCase):
         assert self._wait_value('.reactions ', -1, '0⃣ 1 1⃣ 1 2⃣ 1')
 
     def test_creating_poll_with_over_12_options(self):
+        """Tests if it's not possible to create a poll with more than
+        12 options.
+        """
+
         self.send_message(
             '!poll question?, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15')
 
@@ -66,6 +84,8 @@ class VoteOrDieScriptTestCase(RocketChatTestCase):
 
 
 def main():
+    """The main entry point. """
+
     parser = OptionParser(usage='usage: %prog [options] arguments')
     parser.add_option('-a', '--host', dest='host',
                       help='allows specifying domain or IP of the Rocket.Chat host')
@@ -73,7 +93,7 @@ def main():
                       help='allows specifying admin username')
     parser.add_option('-p', '--password', dest='password',
                       help='allows specifying admin password')
-    options, args = parser.parse_args()
+    options, _ = parser.parse_args()
 
     if not options.host:
         options.host = 'http://127.0.0.1:8006'
